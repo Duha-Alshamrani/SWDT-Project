504e705a6ae31d2f1fc8a3dc5e3155871b7fead8
d4n4h <w4teen09@outlook.com>
2025-05-15 15:00:17 +0000

    Add new patient to the file

---
+++ add_patient.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if there is file argument
+if [ $# -lt 1 ];
+then
+  echo "Error: Missing argument"
+  exit 1
+fi
+
+path="$1"
+
+# Ask user 
+read -p "Do you want to add a new patient? (Y/N): " answer
+
+if [[ "$answer" == "Y" ]];
+then
+    echo "Enter the patient information:"
+    read -p "Name: " Name
+    read -p "ID: " ID
+    read -p "Admission Date (D/M/YYYY): " Admission
+    read -p "Discharge Date (D/M/YYYY): " Discharge
+    read -p "Disease: " Disease
+    read -p "Doctor: " Doctor
+
+    # Append the new patient to the file
+    echo "$Name,$ID,$Admission,$Discharge,$Disease,$Doctor" >> "$path"
+    echo "New patient added successfully."
+    
+else
+    echo "No patient was added."
+fi
